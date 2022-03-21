461e1753cbea8ec873ea624b1b989559c80a211e
sofya-salnikova-nti-johanneberg <94617394+sofya-salnikova-nti-johanneberg@users.noreply.github.com>
2022-03-21 09:05:22 +0100

    Har lagt in Java, och beskrivning i css finns

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 // Denna fil skall innehålla JavaScript (JS)
+var x = 0;
+function openMenu() {
+    if (x == 0) {
+        sideMenuOpen();
+        burgerbtnOn();
+        window.setTimeout(itemContShow, 300);
+        x= 1;
+    } else {
+        itemContInvi();
+        window.setTimeout(sideMenuClose, 300);
+        window.setTimeout(burgerbtnOff, 300);
+        x = 0;
+    }
+}
+
+function sideMenuOpen() {
+    document.getElementById("sideMenu").classList.add("sidemenuwidth");
+}
+
+function sideMenuClose() {
+    document.getElementById("sideMenu").classList.remove("sidemenuwidth");
+}
+
+function burgerbtnOn() {
+    document.getElementById("burgerbtn").setAttribute("class", "blueborder");
+}
+
+function burgerbtnOff() {
+    document.getElementById("burgerbtn").setAttribute("class", "blackborder");
+}
+
+function itemContShow() {
+    document.getElementById("item-container").style.opacity = "1"
+}
+
+function itemContInvi() {
+    document.getElementById("item-container").style.opacity = "0"
+}
